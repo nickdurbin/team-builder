9daa8cfe327c6cd740a90be5ef3816ae8d9e34db
ndurbin <nick.durbin@gmail.com>
2019-09-09 18:20:14 -0400

    Setup the basic form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,7 +6,6 @@ function Form() {
     email: '',
     role: '',
     location: '',
-    bio: ''
   })
 
   const handleChange = event => {
@@ -22,10 +21,25 @@ function Form() {
   } 
 
   return (
-    <div>
-      
+    <div className="team-form">
+      <form onSubmit={event => handleSubmit(event)}>
+        <h1>Fill Out The Form</h1>
+        <label>
+          <input type="text" name="name" value={team.name} placeholder="Name..." onChange={event => handleChange(event)} />
+        </label>
+        <label>
+          <input type="text" name="email" value={team.email} placeholder="Email..." onChange={event => handleChange(event)} />
+        </label>
+        <label>
+          <input type="text" name="role" value={team.role} placeholder="Role..." onChange={event => handleChange(event)} />
+        </label>
+        <label>
+          <input type="text" name="location" placeholder="Location..." value={team.location} onChange={event => handleChange(event)} />
+        </label>
+        <button className="formButton">Submit</button>
+    </form>
     </div>
   )
 }
 
-export default Form
+export default Form;
