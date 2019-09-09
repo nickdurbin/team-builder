f0125a22b8725be169bd1d42ea0eb9eba5a37a86
ndurbin <nick.durbin@gmail.com>
2019-09-09 18:14:21 -0400

    Added the public folder and some basic form component code.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
+  const [team, setTeam] = useState([]);
+
   return (
-    <div className="App">
+    <div>
+      <Route exact path='/' render={props => <Form {...props} /> } />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Form() {
+  const [team, setTeam] = useState({
+    name: '',
+    email: '',
+    role: '',
+    location: '',
+    bio: ''
+  })
+
+  const handleChange = event => {
+    setTeam({
+      ...team,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(event);
+  } 
+
   return (
     <div>
       
