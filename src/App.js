5dc3eebfccf1b765283a8b91cf1de0aaeb74cfdb
nick.durbin <nick.durbin@gmail.com>
2019-09-09 22:23:32 -0400

    I have the data rendering and the styling complete. MVP done.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
 import Form from './components/Form';
+import TeamList from './components/TeamList';
+import { data } from './data';
 
 function App() {
-  const [teamList, setTeamList] = useState([]);
+  const [teamList, setTeamList] = useState([data]);
+  console.log(teamList);
 
   useEffect(() => {
-    console.log(setTeamList(), 'Setting the state to the current list.')
-    setTeamList()
+    setTeamList(data);
   }, [])
   
   return (
     <div className="homePage">
       <Navigation />
-      <Route exact path='/' />
+      <Route exact path='/' render={props => <TeamList {...props} teamList={teamList} /> } />
       <Route path='/form' render={props => <Form {...props} teamList={teamList} /> } />
     </div>
   );
--- src/TeamList.js
+++
@@ -1,6 +0,0 @@
-export const TeamList = {
-  name: 'Nick Durbin',
-  email: 'nick.durbin@gmail.com',
-  role: 'Full-Stack Developer',
-  location: 'Ohio'
-}
